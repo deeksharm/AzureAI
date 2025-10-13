f89b707f9785c58ca394f65a61b9e69dc7de89ce
deeksharm <49231680+deeksharm@users.noreply.github.com>
2025-10-13 10:17:59 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ sk-proj-nPQbPvQKhj5quicvWs_0w1FYNVrUHbCBtp6w2FVrnOV-tnT-xMHyqXam6DiKPpjIOl4SJZ7N
 from openai import OpenAI
  
 client = OpenAI(
-  api_key="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"
+  api_key="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"
 )
  
 response = client.responses.create(
