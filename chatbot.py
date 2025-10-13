d1c7ad2b1c2499e5781747c2c8f16db4f389c70c
deeksharm <49231680+deeksharm@users.noreply.github.com>
2025-10-13 10:11:53 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+ 
+client = OpenAI(
+  api_key="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"
+)
+ 
+response = client.responses.create(
+  model="gpt-5-nano",
+  input="write a haiku about ai",
+  store=True,
+)
+ 
+print(response.output_text);
